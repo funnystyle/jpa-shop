c99584849f051196576274bd277357b9cb16a12b
Funnystyle <esero@hanmail.net>
2022-12-18 01:22:40 +0900

    2-4 쿼리 메소드를 이용한 상품 조회하기

--- src/main/java/org/funnystyle/jpashop/repository/ItemRepository.java
+++ src/main/java/org/funnystyle/jpashop/repository/ItemRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
 }
