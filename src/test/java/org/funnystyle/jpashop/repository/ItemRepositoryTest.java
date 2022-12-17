749057e163246fd717659f4128a140cb59ede228
Funnystyle <esero@hanmail.net>
2022-12-18 02:08:47 +0900

    2-10 JPAQueryFactory를 이용한 상품 조회 예제

--- src/test/java/org/funnystyle/jpashop/repository/ItemRepositoryTest.java
+++ src/test/java/org/funnystyle/jpashop/repository/ItemRepositoryTest.java
@@ -1,13 +1,18 @@
 package org.funnystyle.jpashop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.funnystyle.jpashop.constant.ItemSellStatus;
 import org.funnystyle.jpashop.entity.Item;
+import org.funnystyle.jpashop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,6 +21,9 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
 
     @Autowired
     ItemRepository itemRepository;
@@ -109,4 +117,21 @@ class ItemRepositoryTest {
         }
     }
 
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%" + "테스트 상품 상세 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
+        for (Item item : itemList) {
+            System.out.println(item);
+        }
+    }
 }
