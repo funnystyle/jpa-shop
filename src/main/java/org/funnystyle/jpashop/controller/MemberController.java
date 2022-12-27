9c27d1932b2873fcd4825cfb766fe73de22836af
Funnystyle <esero@hanmail.net>
2022-12-28 01:21:01 +0900

    4-5 회원 가입 페이지 작성하기

---
+++ src/main/java/org/funnystyle/jpashop/controller/MemberController.java
@@ -0,0 +1,23 @@
+package org.funnystyle.jpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.funnystyle.jpashop.dto.MemberFormDto;
+import org.funnystyle.jpashop.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/members")
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/new")
+    public String memberForm(Model model){
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+}
