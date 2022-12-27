c18984c5b9e82c074a84060724904c35fedc2f65
Funnystyle <esero@hanmail.net>
2022-12-28 00:29:35 +0900

    4-2 SecurityConfig 클래스 작성하기

---
+++ src/main/java/org/funnystyle/jpashop/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package org.funnystyle.jpashop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
