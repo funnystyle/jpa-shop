4523ae54ee31b047e1b4f1cff2d44864975907d7
Funnystyle <esero@hanmail.net>
2022-12-28 01:34:03 +0900

    4-6 회원 가입 컨트롤러 소스코드 작성하기

--- src/main/java/org/funnystyle/jpashop/controller/MemberController.java
+++ src/main/java/org/funnystyle/jpashop/controller/MemberController.java
@@ -2,10 +2,13 @@ package org.funnystyle.jpashop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.funnystyle.jpashop.dto.MemberFormDto;
+import org.funnystyle.jpashop.entity.Member;
 import org.funnystyle.jpashop.service.MemberService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
@@ -14,10 +17,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping(value = "/new")
-    public String memberForm(Model model){
+    public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
+
+    @PostMapping(value = "/new")
+    public String newMember(MemberFormDto memberFormDto) {
+        Member member = Member.createMember(memberFormDto, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
+    }
+
 }
