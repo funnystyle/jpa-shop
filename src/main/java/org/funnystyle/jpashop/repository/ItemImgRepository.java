97fbbccdff0ccdea46ba557adb8d9b725940ee31
funnystyle <esero@hanmail.net>
2023-01-02 01:58:29 +0900

    6-1 상품 등록 구현하기

---
+++ src/main/java/org/funnystyle/jpashop/repository/ItemImgRepository.java
@@ -0,0 +1,12 @@
+package org.funnystyle.jpashop.repository;
+
+import org.funnystyle.jpashop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+}
---
+++ src/main/java/org/funnystyle/jpashop/service/ItemService.java
@@ -0,0 +1,48 @@
+package org.funnystyle.jpashop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.funnystyle.jpashop.dto.ItemFormDto;
+import org.funnystyle.jpashop.entity.Item;
+import org.funnystyle.jpashop.entity.ItemImg;
+import org.funnystyle.jpashop.repository.ItemImgRepository;
+import org.funnystyle.jpashop.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    private final ItemImgService itemImgService;
+
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+
+            if (i == 0)
+                itemImg.setRepimgYn("Y");
+            else
+                itemImg.setRepimgYn("N");
+
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+
+}
